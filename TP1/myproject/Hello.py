5fe8acf84dc3427f14b1424e850a1c4e169d0de4
omarsouidi4 <omar.souidi@epfedu.fr>
2023-10-04 14:11:22 +0200

    TP1 fini

--- TP1/myproject/Hello.py
+++ TP1/myproject/Hello.py
@@ -4,4 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    variable="""<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TP5LRMBZ8T"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TP5LRMBZ8T');
+</script>"""
+    return "<p>ALLEZ L'OM!</p>" + variable
